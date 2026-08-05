effcdc892319754229ff3bf1132a7e5f422fad4d
S. Jashwanth Reddy <sjashwanthreddy948@gmail.com>
2026-08-05 22:07:34 +0530

    Time: 193 ms (99.24%) | Memory: 99.8 MB (86.36%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        
+        graph = [[] for _ in range(n)]
+        for u, v in invocations:
+            graph[u].append(v)
+
+      
+        suspicious = [False] * n
+        q = deque([k])
+        suspicious[k] = True
+
+        while q:
+            u = q.popleft()
+            for v in graph[u]:
+                if not suspicious[v]:
+                    suspicious[v] = True
+                    q.append(v)
+
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+        return [i for i in range(n) if not suspicious[i]]
